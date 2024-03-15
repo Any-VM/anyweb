9f880dee301939a339590172646db200758537ae
incognitotgt <tgt@incognitotgt.me>
2024-03-14 23:24:04 -0400

    basic homepage

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,44 +1,49 @@
-/* eslint-disable jsx-a11y/iframe-has-title */
 import type { MetaFunction } from "@remix-run/node";
-import { Settings2 } from "lucide-react";
-import { SunMoon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft, ArrowRight, RotateCw, Settings2, Moon } from "lucide-react";
+import { Input } from "@/components/ui/input";
 
 export const meta: MetaFunction = () => {
-    return [
-        { title: "Anyweb" },
-        { name: "description", content: "Anyweb is an advanced web service for bypassing internet censorship" },
-    ];
+  return [
+    { title: "Anyweb" },
+    {
+      name: "description",
+      content:
+        "Anyweb is an advanced web service for bypassing internet censorship",
+    },
+  ];
 };
 
 export default function Index() {
-    return (
-        <main>
-            <nav className="flex w-screen h-[var(--nav-height)] justify-start">
-                <div className="nav-controls-wrapper flex">
-                    <div className="foward-back-wrapper wrapper-base"></div>
-                    <div className="reload-button-wrapper wrapper-base"></div>
-                </div>
-
-                <input className="search-wrapper flex justify-start align-center flex-grow h-[calc(var(--nav-height)-var(--uni-margin))] m-[calc(var(--uni-margin)*2-var(--uni-margin))] wrapper-base bg-slate-800 bg-opacity-25 uni-border search-effect border-slate-400 focus:outline-none" />
-                
-                <div className="controls-wrapper flex">
-                    <div className="light-dark-wrapper wrapper-base h-[calc(var(--nav-height)-var(--uni-margin))]">
-                        <SunMoon />
-                    </div>
-                    <div className="settings-wrapper wrapper-base h-[calc(var(--nav-height)-var(--uni-margin))] w-[calc(var(--nav-height)-var(--uni-margin))]">
-                        <Settings2 />
-                    </div>
-                </div>
-            </nav>
-
-            <div className="absolute top-[calc(var(--nav-height)+var(--uni-margin))] w-screen uni-border-half wrapper-base h-0"></div>
-            
-            <div className="window-wrapper absolute top-[calc(var(--nav-height)+var(--uni-margin)+var(--border-thickness)/2)] w-screen h-[calc(100vh-var(--uni-margin)*2-var(--nav-height))]">
-                <iframe
-                    src=""
-                    className="flex justify-center uni-border wrapper-base m-[calc(var(--uni-margin)*2-var(--uni-margin))] w-[calc(100vw-var(--uni-margin)*2)] bottom-[var(--uni-margin)] h-[calc(100vh-var(--uni-margin)*3-var(--nav-height)-var(--border-thickness))]"
-                ></iframe>
-            </div>
-        </main>
-    );
+  return (
+    <main className="h-screen overflow-clip">
+      <nav className="fixed w-screen flex h-14 items-center justify-center border-b p-2 z-50 mb-14 bg-background">
+        <div className="justify-start flex-row flex space-x-2">
+          <Button variant="outline" className="size-10 flex flex-col">
+            <ArrowLeft className="text-foreground size-6" />
+          </Button>
+          <Button variant="outline" className="size-10 flex flex-col">
+            <ArrowRight className="text-foreground size-6" />
+          </Button>
+          <Button variant="outline" className="size-10 flex flex-col">
+            <RotateCw className="text-foreground size-6" />
+          </Button>
+        </div>
+        <Input className="m-2" placeholder="Browse the web, uncensored" />
+        <div className="justify-end flex-row flex space-x-2">
+          <Button variant="outline" className="size-10 flex flex-col">
+            <Moon className="text-foreground size-6" />
+          </Button>
+          <Button variant="outline" className="size-10 flex flex-col">
+            <Settings2 className="text-foreground size-6" />
+          </Button>
+        </div>
+      </nav>
+      <iframe
+        src="https://react.dev"
+        className="w-full h-[95vh] translate-y-14"
+        title="Proxied Site"
+      />
+    </main>
+  );
 }
