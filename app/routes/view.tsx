c608a6507efbdff51a7f591dd3cb18f17c2dba4a
incognitotgt <tgt@incognitotgt.me>
2024-03-16 16:33:30 -0400

    changed path, removed weirdness

--- app/routes/view.tsx
+++ app/routes/view.tsx
@@ -7,7 +7,6 @@ import {
   Moon,
   Code,
   ArrowUpRightFromSquare,
-  Slash,
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -35,27 +34,31 @@ export default function Index() {
     <main className="h-screen overflow-clip">
       <nav className="fixed z-50 mb-14 flex h-14 w-screen items-center justify-center border-b bg-background p-2">
         <div className="flex flex-row justify-start space-x-2">
-          <Button variant="outline" className="flex h-10 w-[5.5rem] flex-row">
-            <ArrowLeft className="size-4 text-foreground hover:scale-110 transition-all duration-300 delay-100 mr-1" />
-            <Slash className="size-4 rotate-[-20deg] text-secondary"/>
-            <ArrowRight className="size-4 text-foreground hover:scale-110 transition-all duration-300 delay-100 ml-1" />
+          <Button variant="outline" className="flex size-10 flex-col">
+            <ArrowLeft className="size-4 text-foreground transition-all delay-100 duration-300 hover:scale-110" />
           </Button>
           <Button variant="outline" className="flex size-10 flex-col">
-            <RotateCw className="size-4 text-foreground hover:rotate-[360deg] hover:scale-110 transition-all duration-300 delay-100" />
+            <ArrowRight className="size-4 text-foreground transition-all delay-100 duration-300 hover:scale-110" />
+          </Button>
+          <Button variant="outline" className="flex size-10 flex-col">
+            <RotateCw className="size-4 text-foreground transition-all delay-100 duration-300 hover:rotate-[360deg] hover:scale-110" />
           </Button>
         </div>
         <Input
-          className="m-2 outline-none focus-visible:ring-0 focus-visible:ring-offset-0 hover:bg-secondary hover:bg-opacity-25 focus:bg-secondary focus:bg-opacity-25 hover:border-primary focus:border-primary hover:shadow-[0_0_25px_5px_rgba(30,41,59,0.75)_inset] focus:shadow-[0_0_25px_5px_rgba(30,41,59,0.75)_inset] transition-all duration-300"
+          className="m-2 outline-none transition-all duration-300 hover:shadow-muted focus:border-muted focus:bg-secondary/40 focus:shadow-muted/20"
           placeholder="Browse the web, uncensored"
         />
         <div className="flex flex-row justify-end space-x-2">
           <Button variant="outline" className="flex size-10 flex-col">
-            <Moon className="size-4 text-foreground hover:scale-110 transition-all duration-300 delay-100" />
+            <Moon className="size-4 text-foreground transition-all delay-100 duration-300 hover:scale-110" />
           </Button>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button variant="outline" className="flex size-10 flex-col focus-visible:ring-none focus-visible:ring-transparent">
-                <Settings2 className="size-4 text-foreground hover:scale-110 transition-all duration-300 delay-100 focus-visible:ring-0" />
+              <Button
+                variant="outline"
+                className="focus-visible:ring-none flex size-10 flex-col focus-visible:ring-transparent"
+              >
+                <Settings2 className="size-4 text-foreground transition-all delay-100 duration-300 hover:scale-110 focus-visible:ring-0" />
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-64">
