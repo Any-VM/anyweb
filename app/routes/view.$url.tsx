c413f4ff15d24cde9a84bd71904285045cd4d3ff
Crllect <144494583+crllect@users.noreply.github.com>
2024-03-18 20:16:30 -0400

    tried to add eruda, it works but the button is annoying asf

--- app/routes/view.$url.tsx
+++ app/routes/view.$url.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+
 import {
     ArrowLeft,
     ArrowRight,
@@ -8,6 +9,7 @@ import {
     Code,
     ArrowUpRightFromSquare,
 } from "lucide-react";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
@@ -18,6 +20,7 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+
 export const meta: MetaFunction = () => {
     return [
         { title: "Anyweb" },
@@ -29,7 +32,37 @@ export const meta: MetaFunction = () => {
     ];
 };
 
+import { useState, useEffect, useRef } from "react";
+
 export default function Index() {
+    const [erudaIsToggled, setErudaIsToggled] = useState(false);
+    const erudaRef = useRef(null);
+
+    useEffect(() => {
+        if (erudaIsToggled) {
+            import("eruda").then((eruda) => {
+                if (!erudaRef.current) {
+                    eruda.init({
+                        useShadowDom: true,
+                        autoScale: true,
+                        defaults: {
+                            displaySize: 45,
+                            transparency: 0.8,
+                            theme: "Monokai Pro",
+                        },
+                    });
+                    eruda.show();
+                    erudaRef.current = eruda;
+                } else eruda.show();
+            });
+        } else if (erudaRef.current) {
+            erudaRef.current.destroy();
+            erudaRef.current = null;
+        }
+    }, [erudaIsToggled]);
+
+    const loadEruda = () => setErudaIsToggled(!erudaIsToggled);
+
     return (
         <main className="h-screen overflow-clip">
             <nav className="fixed z-50 mb-14 flex h-14 w-screen items-center justify-center border-b bg-background p-2">
@@ -37,9 +70,11 @@ export default function Index() {
                     <Button variant="outline" className="flex size-10 flex-col">
                         <ArrowLeft className="size-4 text-foreground hover:scale-110" />
                     </Button>
+
                     <Button variant="outline" className="flex size-10 flex-col">
                         <ArrowRight className="size-4 text-foreground hover:scale-110" />
                     </Button>
+
                     <Button variant="outline" className="flex size-10 flex-col">
                         <RotateCw className="size-4 text-foreground hover:scale-110" />
                     </Button>
@@ -48,6 +83,7 @@ export default function Index() {
                     className="m-2 outline-none hover:shadow-muted focus:border-muted focus:bg-secondary/40 focus:shadow-muted/20"
                     placeholder="Browse the web, uncensored"
                 />
+
                 <div className="flex flex-row justify-end space-x-2">
                     <Button variant="outline" className="flex size-10 flex-col">
                         <Moon className="size-4 text-foreground hover:scale-110" />
@@ -61,13 +97,17 @@ export default function Index() {
                                 <Settings2 className="size-4 text-foreground hover:scale-110 focus-visible:ring-0" />
                             </Button>
                         </DropdownMenuTrigger>
+
                         <DropdownMenuContent className="w-64">
                             <DropdownMenuLabel>Settings</DropdownMenuLabel>
+
                             <DropdownMenuSeparator />
-                            <DropdownMenuItem>
+
+                            <DropdownMenuItem onClick={loadEruda}>
                                 <Code className="mr-2 size-4" />
                                 <span>DevTools</span>
                             </DropdownMenuItem>
+
                             <DropdownMenuItem>
                                 <ArrowUpRightFromSquare className="mr-2 size-4" />
                                 <span>Open in new tab</span>
@@ -76,8 +116,9 @@ export default function Index() {
                     </DropdownMenu>
                 </div>
             </nav>
+
             <iframe
-                src="https://react.dev"
+                src=""
                 className="h-[95vh] w-full translate-y-14 select-none"
                 title="Proxied Site"
             />
